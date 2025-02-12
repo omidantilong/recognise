0894a93b7c5b8dc063305879121e560783134174
Omid Kashan <omid@antilong.com>
2025-02-12 21:52:01 +0000

    chore: Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+"use strict";
+/** @type {import("prettier").Config} */
+
+export default {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  printWidth: 100,
+  overrides: [],
+};
