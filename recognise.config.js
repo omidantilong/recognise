a1f80863a59d3019731b0886591feecfcdd495c0
Omid Kashan <omid@antilong.com>
2025-02-13 12:25:43 +0000

    chore: Placeholder config

---
+++ recognise.config.js
@@ -0,0 +1,14 @@
+export default {
+  //projectName: "recognise",
+  //projectOwner: "omidantilong",
+  //repoType: "github",
+  //repoHost: "https://github.com",
+  files: ["README.md"],
+  imageSize: 200,
+  cellsPerRow: 8,
+  // commit: false,
+  // commitConvention: "angular",
+  // contributorsSortAlphabetically: false,
+  // contributorsPerLine: 7,
+  // linkToUsage: false,
+}
