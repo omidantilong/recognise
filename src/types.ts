0bda30c7128ab0bdcc338d1ae8a6b53a731a1eac
Omid Kashan <omid@antilong.com>
2025-02-13 17:33:39 +0000

    chore: Clean up types file

--- src/types.ts
+++ src/types.ts
@@ -9,6 +9,12 @@ export interface PreConfig {
   templates?: Templates
 }
 
+export interface FinalConfig extends Required<PreConfig> {
+  cellWidth: number
+  dictionary: DictionaryKeyed
+  templates: Required<Templates>
+}
+
 export interface Templates {
   contributor?: Function
   contributions?: Function
@@ -16,17 +22,27 @@ export interface Templates {
   table?: Function
 }
 
+// export interface Templates {
+//   contributor?: ({
+//     config,
+//     contributor,
+//     contributions,
+//   }: {
+//     config: FinalConfig
+//     contributor: Contributor
+//     contributions: string
+//   }) => string
+//   contributions?: () => string
+//   row?: () => string
+//   table?: () => string
+// }
+
 export type Dictionary = Array<DictionaryEntry>
 export type DictionaryKeyed = Record<string, DictionaryEntry>
 
 export interface DictionaryEntry {
   icon: string
   text: string
-}
-
-export interface FinalConfig extends Required<PreConfig> {
-  cellWidth: number
-  dictionary: DictionaryKeyed
 }
 
 export interface Contributor {
