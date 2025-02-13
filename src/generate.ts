fc3c15336a4c9dfd57b7e1db90e5ab8127ce7d28
Omid Kashan <omid@antilong.com>
2025-02-13 16:25:39 +0000

    chore: Remove unused imports

--- src/generate.ts
+++ src/generate.ts
@@ -1,7 +1,6 @@
 import { chunk } from "es-toolkit"
 import { outdent } from "outdent"
 import type { FinalConfig, RecoContributor } from "./types"
-import { format } from "prettier"
 
 export async function generate(config: FinalConfig, contributors: RecoContributor[]) {
   const html = {
@@ -21,6 +20,5 @@ export async function generate(config: FinalConfig, contributors: RecoContributo
 
   html.contributors = config.templates.table({ config, rows })
 
-  //return outdent.string(await format(html.contributors, { parser: "html" }))
   return outdent.string(html.contributors)
 }
