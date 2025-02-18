987dd9d59af7a4301b6af4c2d148c7b41fa97523
Omid Kashan <omid@antilong.com>
2025-02-18 21:30:21 +0000

    test: Basic test fixtures

---
+++ test/fixtures/defaultConfig.ts
@@ -0,0 +1,24 @@
+import { FinalConfig } from "../../src/types"
+import { dictionary } from "../../src/dictionary"
+import { loadTemplate } from "../../src/util"
+
+export async function defaultConfig(): Promise<FinalConfig> {
+  return {
+    cellsPerRow: 7,
+    cellWidth: 14.29,
+    dictionary,
+    files: ["README.md"],
+    imageSize: 100,
+    projectName: "recognise",
+    projectOwner: "omidantilong",
+    repoHost: "https://github.com",
+    repoType: "github",
+    sort: "alphabetical",
+    templates: {
+      contributions: await loadTemplate("./templates/contributions"),
+      contributor: await loadTemplate("./templates/contributor"),
+      row: await loadTemplate("./templates/row"),
+      table: await loadTemplate("./templates/table"),
+    },
+  }
+}
